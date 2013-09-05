6fd5113461a1797263e07fa69d05e3ce8b7725f1
alex <alesssandro@ukr.net>
2013-09-05 10:58:01 +0300

    Init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tgallery::Application.config.secret_token = 'f510009620b2be1491c2ee1a4c043cb1a1a96ba3c9ae87f1f6e95916b963ed2d1b951739be1f126a8d064274317ebd2e40694a2ec23e665d3da4d51bd5fdee32'
