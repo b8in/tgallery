6af069a20cd800f2a93b09a0bec20165cad163ff
alex <alesssandro@ukr.net>
2013-09-09 10:37:29 +0300

    configure ImageUploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,7 +13,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{mounted_as.to_s.pluralize}/#{model.g_image_category_id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -32,15 +32,16 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+  version :thumb do
+    process :scale => [50, 50]
+    #process resize_to_fill: [200, 300]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+     %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
@@ -49,3 +50,4 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
 end
+
