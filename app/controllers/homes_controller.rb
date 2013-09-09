405a13415dac27394c8ac009f4b1e558b7f82f45
alex <alesssandro@ukr.net>
2013-09-09 17:03:51 +0300

    fix nav-bar

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
 
   def index
-    @categories = GImageCategory.includes(:g_images).page(params[:page]).per(2)
+    @categories = GImageCategory.includes(:g_images).page(params[:page]).per(5)
   end
 
 end
