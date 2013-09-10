4355f198d65093200c150556268cfd1b708b6113
alex <alesssandro@ukr.net>
2013-09-10 19:19:21 +0300

    redesign root page: show last 6 categories with images

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
 
   def index
-    @categories = GImageCategory.includes(:g_images).page(params[:page]).per(5)
+    @categories = GImageCategory.includes(:g_images).reverse_order.limit(6)
   end
 
 end
