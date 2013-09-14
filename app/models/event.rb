58a3ef1f99c4a3ff3fcf8e6723379250bf9418de
alex <alesssandro@ukr.net>
2013-09-14 15:32:00 +0300

    add spec for model Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ActiveRecord::Base
   attr_accessible :name
 
   has_many :e_histories, dependent: :destroy
+  validates :name,
+            presence: true,
+            uniqueness: { case_sensitive: false},
+            length: { in: 2..60 }
 end
