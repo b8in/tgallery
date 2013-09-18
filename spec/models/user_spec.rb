ecc8c11f67ccd4757e61bff4922b9a5b88da33f4
alex <alesssandro@ukr.net>
2013-09-18 12:09:38 +0300

    append user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,10 @@ describe User do
     it { should allow_mass_assignment_of :password_confirmation }
     it { should allow_mass_assignment_of :remember_me }
     it { should_not allow_mass_assignment_of :id }
+    it { should_not allow_mass_assignment_of :created_at }
+    it { should_not allow_mass_assignment_of :updated_at }
+    it { should_not allow_mass_assignment_of :admin }
+    it { should_not allow_mass_assignment_of :encrypted_password }
   end
 
   describe "data types" do
@@ -39,15 +43,30 @@ describe User do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email).case_insensitive }
     it { should validate_presence_of(:encrypted_password) }
-    it { should validate_presence_of(:admin) }
   end
 
   describe "valid and invalid attributes" do
+    it { should allow_value(true).for(:admin) }
+    it { should allow_value(false).for(:admin) }
     it { should allow_value("usa@mail.com").for(:email) }
     it { should_not allow_value("@mail.com").for(:email) }
     it { should_not allow_value(" @mail.com").for(:email) }
     it { should_not allow_value("usa@mail.x").for(:email) }
     it { should_not allow_value("usa@ma il.com").for(:email) }
+  end
+
+  describe "saving into db" do
+    it "valid object" do
+      user = FactoryGirl.build(:user)
+      user.save.should be_true
+      User.last.should eql(user)
+      FactoryGirl.create(:user)
+    end
+    it "invalid object" do
+      user = FactoryGirl.build(:user, email: nil)
+      user.save.should be_false
+      lambda {user.save!}.should raise_error
+    end
   end
 
 end
