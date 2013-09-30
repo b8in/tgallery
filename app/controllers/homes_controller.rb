e491eec43e957dd4211ad93a31ea0d9b9441e5c6
alex <alesssandro@ukr.net>
2013-09-30 13:46:24 +0300

    configure gon to pass pusher settings to js

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,6 +2,8 @@ class HomesController < ApplicationController
 
   def index
     @categories = GImageCategory.includes(:g_images).reverse_order.limit(6)
+
+    gon.pusher_config = Webs.pusher_config
   end
 
 end
