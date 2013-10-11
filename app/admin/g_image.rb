1b2a1955fdcb0db595b1033232388e53ad5a986a
alex <alesssandro@ukr.net>
2013-10-11 12:31:10 +0300

    fix updating g_image

--- app/admin/g_image.rb
+++ app/admin/g_image.rb
@@ -15,7 +15,7 @@ ActiveAdmin.register GImage, as:'Image' do
       image_tag(img.image.url(:thumb), width:64)
     end
     column :name
-    column 'Category', :g_image_category, sortable: 'g_image_categories.name'
+    column 'Category', :g_image_category, sortable: 'g_image_categories.name'    #important: works with scoped_collection method
     column 'URL' do |img|
       "/public#{img.image.url.to_s}"
     end
@@ -64,9 +64,6 @@ ActiveAdmin.register GImage, as:'Image' do
     end
 
     def update
-
-      puts params.inspect
-
       new_category_id = params[:image][:g_image_category_id]
       @image = GImage.find(params[:id])
       old_category_id = @image.g_image_category_id
@@ -74,51 +71,37 @@ ActiveAdmin.register GImage, as:'Image' do
       if new_category_id.nil?
         new_store_dir = 'without_category'
       else
-        new_store_dir = new_category_id.to_s
+        new_store_dir = GImageCategory.where(id: new_category_id).pluck(:name)[0]
       end
 
       if old_category_id.nil?
         old_store_dir = 'without_category'
       else
-        old_store_dir = old_category_id.to_s
+        old_store_dir = GImageCategory.where(id: old_category_id).pluck(:name)[0]
       end
 
       old_file_path =  "#{Rails.root}/public#{@image.image.url}"
+
       unless @image.update_attributes(params[:image])
         render :edit
       else
-        if old_store_dir != new_store_dir
+        arr = old_file_path.rpartition('/')
+        stored_image_name = arr[2]
+        old_store_dir_path = arr[0]
 
-          #puts "old_file_path = #{old_file_path}"
-
-          arr = old_file_path.rpartition('/')
-          stored_image_name = arr[2]
-          #puts "stored_image_name = #{stored_image_name}"
-          old_store_dir_path = arr[0]
-
-          #puts "old_store_dir_path = #{old_store_dir_path}"
-
-          Dir.chdir(old_store_dir_path)
-
-          #puts "pwd = #{Dir.pwd}"
-
-          pattern = "*"+stored_image_name.to_s
-          #puts pattern
-          moved_files_names = Dir.glob(pattern)
-
-          #puts "moved_files_names = #{moved_files_names.inspect}"
-
-          moved_files_names.each do |fname|
-            new_name = fname.gsub(stored_image_name, @image.name)
-            new_file_path = old_store_dir_path.rpartition('/')[0] + "/#{new_store_dir}/#{new_name}"
-            #puts "new_file_path = #{new_file_path}"
-            FileUtils.mv(old_store_dir_path+'/'+fname, new_file_path)
-          end
-
+        Dir.chdir(old_store_dir_path)
+        pattern = "*"+stored_image_name.to_s
+        moved_files_names = Dir.glob(pattern)
+        moved_files_names.each do |fname|
+          new_name = fname.gsub(stored_image_name, @image.name)
+          new_file_path = old_store_dir_path.rpartition('/')[0] + "/#{new_store_dir}/#{new_name}"
+          FileUtils.mv(old_store_dir_path+'/'+fname, new_file_path)
         end
+
+        @image.image = File.open(old_store_dir_path.rpartition('/')[0] + "/#{new_store_dir}/#{@image.name}")
+        @image.save!
         redirect_to admin_image_path(@image), notice: 'Image was successfully updated.'
       end
-
 
     end
 
@@ -128,14 +111,18 @@ ActiveAdmin.register GImage, as:'Image' do
 
   end
 
-  form  do |f|
+  form do |f|
     f.semantic_errors :base
     f.inputs 'Image Datails' do
       f.input :g_image_category, label: 'Category name:'
       f.input :name, label: 'Image name'
+      if f.object.new_record?
+        f.input :image, as: :file, label: 'Image'
+      end
     end
     f.actions do
-      f.action :submit, label: 'Update', button_html: { disable_with: 'Wait...' }
+      label = f.object.new_record? ? 'Create' : 'Update'
+      f.action :submit, label: label, button_html: { disable_with: 'Wait...' }
       f.action :cancel, label: 'Cancel', wrapper_html: { class: 'cancel' }
     end
   end
