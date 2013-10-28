44c379b4abf1f87cd33956573fa8e35679d08ea2
alex <alesssandro@ukr.net>
2013-10-28 14:06:33 +0200

    update application_ctrl_spec.rb

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -5,17 +5,16 @@ describe ApplicationController do
   describe "private methods" do
 
     before do
-      @user = FactoryGirl.create(:user)
+      #@user = FactoryGirl.create(:user)
       @controller = ApplicationController.new
     end
 
     it "load_categories" do
-      10.times {
-        FactoryGirl.create(:g_image_category)
-      }
+      FactoryGirl.create_list(:g_image_category, 10)
+
       @controller.send(:load_categories)
-      assigns(:categories_menu).should_not be_nil
-      assigns(:categories_menu).should have(10).categories
+      expect(assigns(:categories_menu)).not_to be_nil
+      expect(assigns(:categories_menu)).to have(10).categories
     end
 
   end
