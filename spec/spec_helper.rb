dc6e29c8b78db7c1bab378843324b780ebd5e77a
alex <alesssandro@ukr.net>
2013-11-13 10:30:08 +0200

    rm commented code from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,9 +15,6 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
 
-  #config.include Warden::Test::Helpers
-  #config.include Devise::TestHelpers, type: :controller
-
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
